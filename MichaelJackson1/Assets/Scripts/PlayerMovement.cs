59c82c22a48ccc00e2b40a142c4e45cca1b6b1c7
Mark Hesseling <mark.hesseling@outlook.com>
2023-10-22 17:43:33 +0200

    part of movement

---
+++ MichaelJackson1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator animator;
+
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
