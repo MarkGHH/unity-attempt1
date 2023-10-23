f2203f33727a3923ae58dc5a422852ff2f429faa
Mark Hesseling <mark.hesseling@outlook.com>
2023-10-23 20:17:15 +0200

    big mess

--- MichaelJackson1/Assets/Scripts/PlayerMovement.cs
+++ MichaelJackson1/Assets/Scripts/PlayerMovement.cs
@@ -2,25 +2,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    [SerializeField] public float mSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
     public Animator animator;
+    private SpriteRenderer sprite;
 
-    void Update()
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>(); 
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void Update()
     {
         //Input sets horizontal/vertical to a value of -1.0 to 1.0, animator uses these values within the parameters to set an animation
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-    }
+        animator.SetFloat("mSpeed", mSpeed);
+        /*  if (movement.x > 0f)
+          {
+              sprite.flipX = true;    
+          }
+          else if (movement.x < 0f) 
+          {
+              sprite.flipX = false; 
+          }*/
 
+    }
+/*
     private void FixedUpdate()
     {
         //Movement - FixedUpdate ensures that it is unrelated to FPS and instead runs +- 50 times per second, further optimzed with deltatime
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-    }
+        rb.MovePosition(rb.position + movement * mSpeed * Time.fixedDeltaTime);
+    }*/
 }
