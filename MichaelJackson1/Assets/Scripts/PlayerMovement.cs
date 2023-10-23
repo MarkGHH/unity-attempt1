04e0319b42abf5260daf53711128f1c1dee29887
Mark Hesseling <mark.hesseling@outlook.com>
2023-10-23 23:47:05 +0200

    movement update
    idle missing, fix next time

--- MichaelJackson1/Assets/Scripts/PlayerMovement.cs
+++ MichaelJackson1/Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,71 @@
+using System.Numerics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] public float mSpeed = 5f;
+    [SerializeField] public float walkSpeed = 4.0f;
+    [SerializeField] public float runSpeed = 8.0f;
+    public float speed;
     public Rigidbody2D rb;
-    Vector2 movement;
     public Animator animator;
     private SpriteRenderer sprite;
+    [SerializeField] private float horizontal;
+    [SerializeField] private float vertical;
+    public bool isWalking;
+    public bool isRunning;
+    public bool isIdle;
+
 
     private void Start()
     {
+        //GetComponent instantiate
         sprite = GetComponent<SpriteRenderer>(); 
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
     public void Update()
     {
-        //Input sets horizontal/vertical to a value of -1.0 to 1.0, animator uses these values within the parameters to set an animation
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("mSpeed", mSpeed);
-          if (movement.x > 0f)
-          {
-              sprite.flipX = true;    
-          }
-          else if (movement.x < 0f) 
-          {
-              sprite.flipX = false; 
-          }
+        //Is the player moving based on X/Y input
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
 
-    }
+        if (horizontal != 0 || vertical != 0)
+        {
+            //Set animator parameters based on current speed
+            animator.SetFloat("Horizontal", horizontal);
+            animator.SetFloat("Vertical", vertical);
+            animator.SetFloat("Speed", speed);
 
-    private void FixedUpdate()
-    {
-        //Movement - FixedUpdate ensures that it is unrelated to FPS and instead runs +- 50 times per second, further optimzed with deltatime
-        rb.MovePosition(rb.position + movement * mSpeed * Time.fixedDeltaTime);
+            //Handle direction swap in animator
+            if (horizontal > 0f)
+            {
+                sprite.flipX = true;
+            }
+            else
+            {
+                sprite.flipX = false;
+            }
+        }
+        else
+        {
+            isIdle = true;
+        }
+
+        //If Shift (run) is being pressed, movement speed is doubled
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            isRunning = true;
+        }
+        else
+        {
+            speed = walkSpeed;
+            isWalking = true;
+        }
+        rb.velocity = new UnityEngine.Vector2(horizontal * speed, vertical * speed);
+        speed = rb.velocity.magnitude;
     }
+    //change speed parameter to isMoving, isWalking, isIdle 
+    //still use speed as velocity
+    //set proper animations based on speed
 }
